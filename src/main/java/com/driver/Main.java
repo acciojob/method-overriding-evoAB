81fffe678a080dd274f96cdcddd84a03f20874ac
Abhishek <abhi@gmail.com>
2023-12-14 00:09:33 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A a = new A();
-        System.out.println(a.meth());
-
         B b = new B();
         System.out.println(b.meth());
+
+        B bb = new B();
+        System.out.println(bb.meth());
     }
 }
