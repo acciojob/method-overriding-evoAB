c22113a95e4d0b21e05d62781a5bdfe8f9ff9428
Abhishek <abhi@gmail.com>
2023-12-13 23:55:32 +0530

    Added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,6 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        A a = new A();
+        System.out.println(a.meth());
 
         B b = new B();
         System.out.println(b.meth());
